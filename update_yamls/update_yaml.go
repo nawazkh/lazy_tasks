61277f8e1e821ca5a63d5876eb85704183897a9c
Nawaz Hussain Khazielakha <k.nawaz.h@gmail.com>
2024-07-11 19:18:27 -0700

    update update_yaml script

--- update_yamls/update_yaml.go
+++ update_yamls/update_yaml.go
@@ -34,16 +34,35 @@ var (
 	allJobs           []string
 )
 
-type Presubmit struct {
+//type Presubmit struct {
+//	Name              string            `yaml:"name" json:"name"`
+//	Cluster           string            `yaml:"cluster,omitempty" json:"cluster,omitempty"`
+//	PathAlias         string            `yaml:"path_alias,omitempty" json:"path_alias"`
+//	Optional          bool              `yaml:"optional" json:"optional"`
+//	Decorate          bool              `yaml:"decorate,omitempty" json:"decorate"`
+//	DecorationConfig  DecorationConfig  `yaml:"decoration_config,omitempty" json:"decoration_config,omitempty"`
+//	SkipIfOnlyChanged string            `yaml:"skip_if_only_changed,omitempty" json:"skip_if_only_changed,omitempty"`
+//	RunIfChanged      string            `yaml:"run_if_changed,omitempty" json:"run_if_changed,omitempty"`
+//	AlwaysRun         bool              `yaml:"always_run" json:"always_run"`
+//	MaxConcurrency    int               `yaml:"max_concurrency,omitempty" json:"max_concurrency,omitempty"`
+//	Labels            map[string]string `yaml:"labels,omitempty" json:"labels,omitempty"`
+//	Branches          []string          `yaml:"branches,omitempty" json:"branches"`
+//	ExtraRefs         []ExtraRef        `yaml:"extra_refs,omitempty" json:"extra_refs"`
+//	Spec              Spec              `yaml:"spec,omitempty" json:"spec"`
+//	Annotations       TestAnnotation    `yaml:"annotations,omitempty" json:"annotations"`
+//}
+
+type Periodic struct {
 	Name              string            `yaml:"name" json:"name"`
 	Cluster           string            `yaml:"cluster,omitempty" json:"cluster,omitempty"`
 	PathAlias         string            `yaml:"path_alias,omitempty" json:"path_alias"`
-	Optional          bool              `yaml:"optional" json:"optional"`
+	Optional          bool              `yaml:"optional,omitempty" json:"optional"`
 	Decorate          bool              `yaml:"decorate,omitempty" json:"decorate"`
 	DecorationConfig  DecorationConfig  `yaml:"decoration_config,omitempty" json:"decoration_config,omitempty"`
+	Interval          string            `yaml:"interval,omitempty" json:"interval,omitempty"`
 	SkipIfOnlyChanged string            `yaml:"skip_if_only_changed,omitempty" json:"skip_if_only_changed,omitempty"`
 	RunIfChanged      string            `yaml:"run_if_changed,omitempty" json:"run_if_changed,omitempty"`
-	AlwaysRun         bool              `yaml:"always_run" json:"always_run"`
+	AlwaysRun         bool              `yaml:"always_run,omitempty" json:"always_run"`
 	MaxConcurrency    int               `yaml:"max_concurrency,omitempty" json:"max_concurrency,omitempty"`
 	Labels            map[string]string `yaml:"labels,omitempty" json:"labels,omitempty"`
 	Branches          []string          `yaml:"branches,omitempty" json:"branches"`
@@ -96,8 +115,12 @@ type DecorationConfig struct {
 	Timeout string `yaml:"timeout,omitempty" json:"timeout,omitempty"`
 }
 
+//type Config struct {
+//	Presubmits map[string][]Presubmit `yaml:"presubmits" json:"presubmits"`
+//}
+
 type Config struct {
-	Presubmits map[string][]Presubmit `yaml:"presubmits" json:"presubmits"`
+	Periodics []Periodic `yaml:"periodics" json:"periodics"`
 }
 
 type TestAnnotation struct {
@@ -166,86 +189,34 @@ func PrintAllJobs() {
 }
 
 // MigrateSpecToWi migrates the spec to -wi
-func MigrateSpecToWi(config Config) Config {
-	newPresubmits := make(map[string][]Presubmit)
-	for repo, jobs := range config.Presubmits {
-		for _, oldJob := range jobs {
+func MigrateSpecToWiPeriodics(config Config) Config {
+	newPresubmits := []Periodic{}
+	for _, oldJob := range config.Periodics {
 
-			// store all the jobs
-			allJobs = append(allJobs, oldJob.Name)
+		// store all the jobs
+		allJobs = append(allJobs, oldJob.Name)
 
-			// work on creating a new spec only if it has the specified presets
-			if oldJob.Labels["preset-azure-cred-only"] == "true" || oldJob.Labels["preset-azure-capz-sa-cred"] == "true" {
+		// Create a new spec only if it has the specified presets
+		if oldJob.Labels["preset-azure-cred-only"] == "true" || oldJob.Labels["preset-azure-capz-sa-cred"] == "true" {
 
-				// exit if the job has more than 1 container
-				numContainers := len(oldJob.Spec.Containers)
-				if numContainers > 1 {
-					log.Fatalf("Job %s has more than 1 container", oldJob.Name)
-				}
-
-				isE2E := false
-				for _, env := range oldJob.Spec.Containers[0].Args {
-					if strings.Contains(env, "ci-e2e.sh") {
-						isE2E = true
-					}
-				}
-
-				// migrate only e2e jobs
-				if isE2E {
-					newJob := oldJob
-
-					// remove old labels
-					newJob.Labels = make(map[string]string)
-					for k, v := range oldJob.Labels {
-						if _, ok := newJob.Labels["preset-azure-cred-only"]; ok {
-							continue
-						}
-						if _, ok := newJob.Labels["preset-azure-capz-sa-cred"]; ok {
-							continue
-						}
-						newJob.Labels[k] = v
-					}
-
-					// add new label preset-azure-cred-wi
-					newJob.Labels["preset-azure-cred-wi"] = "true"
-
-					// add new serviceAccountName: prowjob-default-sa to spec
-					newJob.Spec.ServiceAccountName = "prowjob-default-sa"
-
-					// save to newPresubmits
-					newPresubmits[repo] = append(newPresubmits[repo], newJob)
-				} else {
-					// do not migrate non-e2e jobs, ie. conformance jobs for now
-					// save to newPresubmits
-					newPresubmits[repo] = append(newPresubmits[repo], oldJob)
-				}
-				// save the list of jobs being copied over even if they are not migrated right now.
-				// TODO: wait for conformance jobs to be green
-				copiedOverJobs = append(copiedOverJobs, oldJob.Name)
-			} else {
-				notCopiedOverJobs = append(notCopiedOverJobs, oldJob.Name)
+			// exit if the job has more than 1 container
+			numContainers := len(oldJob.Spec.Containers)
+			if numContainers > 1 {
+				log.Fatalf("Job %s has more than 1 container", oldJob.Name)
 			}
-		}
-	}
-	return Config{newPresubmits}
-}
 
-// CheckAndCreateSpec checks for the specified presets and creates a new spec if they are found
-func CheckAndCreateSpec(config Config) Config {
-	newPresubmits := make(map[string][]Presubmit)
-	for repo, jobs := range config.Presubmits {
-		for _, oldJob := range jobs {
-			newPresubmits[repo] = append(newPresubmits[repo], oldJob)
-			allJobs = append(allJobs, oldJob.Name)
+			isE2E := false
+			for _, env := range oldJob.Spec.Containers[0].Args {
+				if strings.Contains(env, "ci-e2e.sh") {
+					isE2E = true
+				}
+			}
 
-			// work on creating a new spec
-			if oldJob.Labels["preset-azure-cred-only"] == "true" || oldJob.Labels["preset-azure-capz-sa-cred"] == "true" {
+			// migrate only e2e jobs
+			if isE2E {
 				newJob := oldJob
 
-				// add a -wi to its name
-				newJob.Name = oldJob.Name + "-wi"
-
-				// update labels
+				// remove old labels
 				newJob.Labels = make(map[string]string)
 				for k, v := range oldJob.Labels {
 					if _, ok := newJob.Labels["preset-azure-cred-only"]; ok {
@@ -257,30 +228,82 @@ func CheckAndCreateSpec(config Config) Config {
 					newJob.Labels[k] = v
 				}
 
-				// add new preset-azure-cred-wi label
+				// add new label preset-azure-cred-wi
 				newJob.Labels["preset-azure-cred-wi"] = "true"
 
 				// add new serviceAccountName: prowjob-default-sa to spec
 				newJob.Spec.ServiceAccountName = "prowjob-default-sa"
 
-				// update testgrid tab name
-				newJob.Annotations = TestAnnotation{
-					TestgridDashboards: oldJob.Annotations.TestgridDashboards,
-					TestgridTabName:    oldJob.Annotations.TestgridTabName + "-wi",
-					TestgridAlertEmail: oldJob.Annotations.TestgridAlertEmail,
-					Description:        oldJob.Annotations.Description,
-				}
-
 				// save to newPresubmits
-				newPresubmits[repo] = append(newPresubmits[repo], newJob)
-				copiedOverJobs = append(copiedOverJobs, oldJob.Name)
+				newPresubmits = append(newPresubmits, newJob)
 			} else {
-				notCopiedOverJobs = append(notCopiedOverJobs, oldJob.Name)
+				// do not migrate non-e2e jobs, ie. conformance jobs for now
+				// save to newPresubmits
+				newPresubmits = append(newPresubmits, oldJob)
 			}
+			// save the list of jobs being copied over even if they are not migrated right now.
+			// TODO: wait for conformance jobs to be green
+			copiedOverJobs = append(copiedOverJobs, oldJob.Name)
+		} else {
+			notCopiedOverJobs = append(notCopiedOverJobs, oldJob.Name)
+			newPresubmits = append(newPresubmits, oldJob)
 		}
 	}
+
 	return Config{newPresubmits}
 }
+
+// CheckAndCreateSpec checks for the specified presets and creates a new spec if they are found
+//func CheckAndCreateSpec(config Config) Config {
+//	newPresubmits := make(map[string][]Presubmit)
+//	for repo, jobs := range config.Presubmits {
+//		for _, oldJob := range jobs {
+//			newPresubmits[repo] = append(newPresubmits[repo], oldJob)
+//			allJobs = append(allJobs, oldJob.Name)
+//
+//			// work on creating a new spec
+//			if oldJob.Labels["preset-azure-cred-only"] == "true" || oldJob.Labels["preset-azure-capz-sa-cred"] == "true" {
+//				newJob := oldJob
+//
+//				// add a -wi to its name
+//				newJob.Name = oldJob.Name + "-wi"
+//
+//				// update labels
+//				newJob.Labels = make(map[string]string)
+//				for k, v := range oldJob.Labels {
+//					if _, ok := newJob.Labels["preset-azure-cred-only"]; ok {
+//						continue
+//					}
+//					if _, ok := newJob.Labels["preset-azure-capz-sa-cred"]; ok {
+//						continue
+//					}
+//					newJob.Labels[k] = v
+//				}
+//
+//				// add new preset-azure-cred-wi label
+//				newJob.Labels["preset-azure-cred-wi"] = "true"
+//
+//				// add new serviceAccountName: prowjob-default-sa to spec
+//				newJob.Spec.ServiceAccountName = "prowjob-default-sa"
+//
+//				// update testgrid tab name
+//				newJob.Annotations = TestAnnotation{
+//					TestgridDashboards: oldJob.Annotations.TestgridDashboards,
+//					TestgridTabName:    oldJob.Annotations.TestgridTabName + "-wi",
+//					TestgridAlertEmail: oldJob.Annotations.TestgridAlertEmail,
+//					Description:        oldJob.Annotations.Description,
+//				}
+//
+//				// save to newPresubmits
+//				newPresubmits[repo] = append(newPresubmits[repo], newJob)
+//				copiedOverJobs = append(copiedOverJobs, oldJob.Name)
+//			} else {
+//				notCopiedOverJobs = append(notCopiedOverJobs, oldJob.Name)
+//			}
+//		}
+//	}
+//	return Config{newPresubmits}
+//}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -296,11 +319,13 @@ func main() {
 		log.Fatalf("Failed to load YAML file: %v", err)
 	}
 
-	log.Println("Following jobs will be updated:")
-	newConfig := CheckAndCreateSpec(presubmits)
-
+	// TO copy over jobs
 	//log.Println("Following jobs will be updated:")
-	//newConfig := MigrateSpecToWi(presubmits)
+	//newConfig := CheckAndCreateSpec(presubmits)
+
+	// To Migrate to wi
+	log.Println("Following jobs will be updated:")
+	newConfig := MigrateSpecToWiPeriodics(presubmits)
 
 	err = SaveYAML(filename, newConfig)
 	if err != nil {
